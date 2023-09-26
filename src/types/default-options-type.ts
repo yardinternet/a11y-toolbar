5796e0bf130e5f602c598d8f68eaf164e064efe4
Lara van Diemen <lara@yard.nl>
2023-09-26 15:23:19 +0200

    (feat): add translate script

--- src/types/default-options-type.ts
+++ src/types/default-options-type.ts
@@ -4,6 +4,7 @@ export interface DefaultOptionsType {
 	showContrastButton?: boolean;
 	showPrintButton?: boolean;
 	showLanguageButton?: boolean;
+	translateIncludedLanguages?: string;
 	readSpeakerCustomerID?: string;
 	readSpeakerContentID?: string;
 	iconOptions?: {
---
+++ src/utils/isScriptLoaded.ts
@@ -0,0 +1,18 @@
+/**
+ * Checks if a script source is already loaded
+ *
+ * @param {string} scriptSrc - The script source to check.
+ */
+export const isScriptLoaded = (scriptSrc: string): boolean => {
+	const scriptEntries = performance
+		.getEntriesByType('resource')
+		.filter((e: any) => e.initiatorType === 'script');
+
+	for (const entry of scriptEntries) {
+		if (entry.name.includes(scriptSrc) && entry.duration > 0) {
+			return true;
+		}
+	}
+
+	return false;
+};
